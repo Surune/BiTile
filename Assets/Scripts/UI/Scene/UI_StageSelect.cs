6309cfb70c1edcdec9fd610b82c3e278a4534d8a
Surune <seojk413@naver.com>
2026-06-17 05:38:50 +0900

    Add editor controls for stage selection

--- Assets/Scripts/UI/Scene/UI_StageSelect.cs
+++ Assets/Scripts/UI/Scene/UI_StageSelect.cs
@@ -2,16 +2,71 @@ using UnityEngine;
 
 public class UI_StageSelect : MonoBehaviour
 {
+    private const string StagePlayerPrefsKey = "STAGE";
+    private const int FirstStage = 1;
+    private const int LastStage = 200;
+
     [SerializeField] private Transform stageContainer;
     [SerializeField] private UI_World_Stage stagePrefab;
 
+#if UNITY_EDITOR
+    private string editorLastUnlockedStageText;
+#endif
+
     private void Awake()
     {
-        var clearedStage = PlayerPrefs.GetInt("STAGE", 1);
-        for (var i = 1; i < 200; i++)
+        RefreshStages();
+
+#if UNITY_EDITOR
+        editorLastUnlockedStageText = PlayerPrefs.GetInt(StagePlayerPrefsKey, FirstStage).ToString();
+#endif
+    }
+
+    private void RefreshStages()
+    {
+        foreach (Transform child in stageContainer)
+        {
+            Destroy(child.gameObject);
+        }
+
+        var clearedStage = PlayerPrefs.GetInt(StagePlayerPrefsKey, FirstStage);
+        for (var i = FirstStage; i <= LastStage; i++)
         {
             var stage = Instantiate(stagePrefab, stageContainer).GetComponent<UI_World_Stage>();
             stage.SetInfo(i, clearedStage);
         }
     }
+
+#if UNITY_EDITOR
+    private void OnGUI()
+    {
+        GUILayout.BeginVertical(GUI.skin.box, GUILayout.Width(260f));
+        GUILayout.Label("Stage Selection Unlock");
+        GUILayout.Label($"Current Last Unlocked: {PlayerPrefs.GetInt(StagePlayerPrefsKey, FirstStage)}");
+        editorLastUnlockedStageText = GUILayout.TextField(editorLastUnlockedStageText, GUILayout.Width(120f));
+
+        GUILayout.BeginHorizontal();
+        if (GUILayout.Button("Apply"))
+        {
+            ApplyEditorLastUnlockedStage();
+        }
+
+        if (GUILayout.Button("Reset"))
+        {
+            editorLastUnlockedStageText = FirstStage.ToString();
+            ApplyEditorLastUnlockedStage();
+        }
+        GUILayout.EndHorizontal();
+        GUILayout.EndVertical();
+    }
+
+    private void ApplyEditorLastUnlockedStage()
+    {
+        var lastUnlockedStage = Mathf.Clamp(int.Parse(editorLastUnlockedStageText), FirstStage, LastStage);
+        editorLastUnlockedStageText = lastUnlockedStage.ToString();
+        PlayerPrefs.SetInt(StagePlayerPrefsKey, lastUnlockedStage);
+        PlayerPrefs.Save();
+        RefreshStages();
+    }
+#endif
 }
