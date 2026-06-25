384b5dc50beb0d30abbcaa11a3f753bc4feda0ab
Surune <seojk413@naver.com>
2026-06-25 13:31:31 +0900

    change stage select scene color

--- Assets/Scripts/UI/Scene/UI_ChapterSelect.cs
+++ Assets/Scripts/UI/Scene/UI_ChapterSelect.cs
@@ -8,15 +8,18 @@ public class UI_ChapterSelect : MonoBehaviour
     private const float ChapterExitDistance = 7f;
 
     [SerializeField] private UI_ChapterCarousel chapterCarousel;
+    [SerializeField] private Camera backgroundCamera;
     [SerializeField] private float chapterExitDistance = ChapterExitDistance;
 
     private bool isTransitioning;
     private Vector3 chapterContentDefaultPosition;
+    private Color defaultBackgroundColor;
 
     private void Awake()
     {
         chapterCarousel.Init(this);
         chapterContentDefaultPosition = chapterCarousel.transform.localPosition;
+        defaultBackgroundColor = backgroundCamera.backgroundColor;
     }
 
     public void SelectChapter(int chapter)
@@ -59,9 +62,12 @@ public class UI_ChapterSelect : MonoBehaviour
 
         var chapterContent = chapterCarousel.transform;
         chapterContent.DOKill();
+        backgroundCamera.DOKill();
         chapterContent.localPosition = chapterContentDefaultPosition;
+        var chapterColor = GameManager.Instance.Chapter.GetData(GameManager.Instance.StageSelection.Chapter).BackgroundColor;
         var sequence = DOTween.Sequence();
         sequence.Join(chapterContent.DOLocalMoveY(chapterContentDefaultPosition.y + GetChapterExitDistance(), TransitionDuration).SetEase(Ease.InOutCubic));
+        sequence.Join(backgroundCamera.DOColor(chapterColor, TransitionDuration).SetEase(Ease.InOutCubic));
         sequence.Join(stageSelect.PlayIntroTransition(TransitionDuration));
         sequence.OnComplete(() => isTransitioning = false);
     }
@@ -71,10 +77,13 @@ public class UI_ChapterSelect : MonoBehaviour
         isTransitioning = true;
         var chapterContent = chapterCarousel.transform;
         chapterContent.DOKill();
+        backgroundCamera.DOKill();
         chapterContent.localPosition = chapterContentDefaultPosition + Vector3.up * GetChapterExitDistance();
-        return chapterContent.DOLocalMove(chapterContentDefaultPosition, duration)
-            .SetEase(Ease.InOutCubic)
-            .OnComplete(() => isTransitioning = false);
+        var sequence = DOTween.Sequence();
+        sequence.Join(chapterContent.DOLocalMove(chapterContentDefaultPosition, duration).SetEase(Ease.InOutCubic));
+        sequence.Join(backgroundCamera.DOColor(defaultBackgroundColor, duration).SetEase(Ease.InOutCubic));
+        sequence.OnComplete(() => isTransitioning = false);
+        return sequence;
     }
 
     private float GetChapterExitDistance()
