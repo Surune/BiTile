22c2246c4e5e313620a2d78baa88cdc3f270474d
Surune <seojk413@naver.com>
2026-08-19 20:25:28 +0900

    change logo

--- Assets/Scripts/UI/Scene/UI_Logo.cs
+++ Assets/Scripts/UI/Scene/UI_Logo.cs
@@ -9,6 +9,7 @@ public class UI_Logo : MonoBehaviour
     [SerializeField] private Image logoImage;
     [SerializeField] private float powerOnDelay = 1.25f;
     [SerializeField] private float powerOffDelay = 2f;
+    [SerializeField] private float fadeInDuration = 0.2f;
     [SerializeField] private float fadeOutDuration = 0.2f;
     [SerializeField] private float sceneMovementDelay = 0.2f;
 
@@ -20,9 +21,10 @@ public class UI_Logo : MonoBehaviour
 
     private async Task Display()
     {
-        GameManager.Instance.Sound.PlaySFX(Definitions.SoundType.Logo);
         await Task.Delay(powerOnDelay.ToMilliseconds());
         logoImage.gameObject.SetActive(true);
+        logoImage.color = new Color(1f, 1f, 1f, 0f);
+        await logoImage.DOFade(1f, fadeInDuration).AsyncWaitForCompletion();
         await Task.Delay(powerOffDelay.ToMilliseconds());
         await logoImage.DOFade(0f, fadeOutDuration).AsyncWaitForCompletion();
         await Task.Delay(sceneMovementDelay.ToMilliseconds());
